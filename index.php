79c09ef1a1f62a549de42259ac73fd8e49de887e
pradnyashirale <pradnyashirale@gmail.com>
2018-06-25 17:10:13 +0530

    my comit

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="page">
+		<div class="header">
+			<h2>ROOMWISE</h2>
+
+			<ul class="menu">
+				<li class="li"><a href="#">Home</a></li>
+				<li class="li"><a href="#">About</a></li>
+				<li class="li"><a href="#">Contact</a></li>
+			</ul>
+
+		</div>
+		<div>
+		<img class="slider" src="Desert.jpg">
+		</div>
+		<div class="sider">
+		<ul>
+			<li><a href="state.php">STATE</a></li>
+			<li><a href="city.php">CITY</a></li>
+			<li><a href=""></a></li>
+			<li><a href=""></a></li>
+			<li><a href=""></a></li>
+		</ul>
+	</div>
+	
+	</div>
+</body>
+</html>
